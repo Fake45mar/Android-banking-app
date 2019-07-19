aa50eb9bec3feaecf2ee1dfc9e5be9a685d19f69
Lev <38050410+Fake45mar@users.noreply.github.com>
2019-07-19 12:25:17 +0300

    Added first version and fixed first download currency

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends Activity {
+    protected Map<String, String> counts = new HashMap<>();
+    private requestBtc req = new requestBtc("Requesting currency");
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setContentView(R.layout.main);
+        final TextView btc = (TextView) findViewById(R.id.courseBtc);
+        final TextView usd = (TextView) findViewById(R.id.courseUsd);
+        final TextView eur = (TextView) findViewById(R.id.courseEur);
+        final TextView gbp = (TextView) findViewById(R.id.courseGbp);
+        Button upd = (Button) findViewById(R.id.updateCourse);
+        try {
+            req.start();
+            btc.setText(counts.get("BTC"));
+            usd.setText(counts.get("USD"));
+            eur.setText(counts.get("EUR"));
+            gbp.setText(counts.get("GBP"));
+        }
+        catch(Exception e){
+            System.out.println(e + "ERROR404");
+            req.interrupt();
+        }
+        upd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    req.start();
+                    btc.setText(counts.get("BTC"));
+                    usd.setText(counts.get("USD"));
+                    eur.setText(counts.get("EUR"));
+                    gbp.setText(counts.get("GBP"));
+                }
+                catch(Exception e){
+                    System.out.println(e + "ERROR414");
+                    req.interrupt();
+                }
+            }
+        });
+    }
+    class requestBtc extends Thread {
+        private Thread t;
+        private String threadName;
+        public requestBtc(String threadName){
+            this.threadName = threadName;
+        }
+        public void run(){
+            try {
+                simpleBank request = new simpleBank();
+                String btc = request.reqestCrpitoCurrency();
+                String usd = request.getCurrencyUSD();
+                String eur = request.getCurrencyEUR();
+                String gbp = request.getCurrencyGBP();
+                counts.clear();
+                counts.put("BTC", btc);
+                counts.put("USD", usd);
+                counts.put("EUR", eur);
+                counts.put("GBP", gbp);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        public void start(){
+            if (t == null){
+                try {
+                    t = new Thread(this, threadName);
+                    t.start();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/simpleBank.java
@@ -0,0 +1,124 @@
+package com.example.myapplication;
+import org.json.JSONObject;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class simpleBank {
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+    private String date = sdf.format(new Date());
+    private final String USER_AGENT = "Mozilla/5.0";
+    private URL obj;
+    private HttpURLConnection con;
+    private HttpsURLConnection conn;
+    private BufferedReader requestResponse;
+    private DataOutputStream wr;
+    private JSONObject jsonCrpito;
+    private JSONObject currency;
+    private JSONObject USDcurrency;
+    private JSONObject EURcurrency;
+    private JSONObject GBPcurrency;
+    private JSONObject jsonCurseGBP;
+    private JSONObject jsonCurseUSD;
+    private JSONObject jsonCurseEUR;
+    private String GBP;
+    private String EUR;
+    private String USD;
+
+    public String reqestCrpitoCurrency() throws Exception{
+        String url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+        obj = new URL(url);
+        con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code " + responseCode);
+        requestResponse = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        String responseString;
+        while((responseString = requestResponse.readLine()) != null){
+            response.append(responseString);
+        }
+        jsonCrpito = new JSONObject(response.toString());
+        currency = new JSONObject(jsonCrpito.get("bpi").toString());
+        USDcurrency = new JSONObject(currency.get("USD").toString());
+        USD = USDcurrency.get("rate").toString();
+        EURcurrency = new JSONObject(currency.get("EUR").toString());
+        EUR = EURcurrency.get("rate").toString();
+        GBPcurrency = new JSONObject(currency.get("GBP").toString());
+        GBP = GBPcurrency.get("rate").toString();
+        USD = USD.substring(0, USD.length() - 5);
+        return (USD + " USD");
+    }
+
+    public String getCurrencyEUR() throws Exception{
+        String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?" + "valcode=EUR" + "&date=" + date + "&json";
+        System.out.println(url);
+        obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = conn.getResponseCode();
+        System.out.println(responseCode);
+        requestResponse = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        String input;
+        while((input = requestResponse.readLine()) != null){
+            response.append(input);
+        }
+        response.deleteCharAt(0);
+        response.deleteCharAt(response.length() - 1);
+        jsonCurseEUR = new JSONObject(response.toString());
+        return (jsonCurseEUR.get("rate").toString().substring(0, jsonCurseEUR.get("rate").toString().length() - 4));
+//        System.out.println(response.toString());
+    }
+
+    public String getCurrencyUSD() throws Exception{
+        String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?" + "valcode=USD" + "&date=" + date + "&json";
+        System.out.println(url);
+        obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code " + responseCode);
+        requestResponse = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        String input;
+        while((input = requestResponse.readLine()) != null){
+            response.append(input);
+        }
+        response.deleteCharAt(0);
+        response.deleteCharAt(response.length() - 1);
+        jsonCurseUSD = new JSONObject(response.toString());
+        return(jsonCurseUSD.get("rate").toString().substring(0, jsonCurseUSD.get("rate").toString().length() - 4));
+//        System.out.println(response.toString());
+    }
+
+    public String getCurrencyGBP() throws Exception{
+        String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?" + "valcode=GBP" + "&date=" + date + "&json";
+        System.out.println(url);
+        obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code " + responseCode);
+        requestResponse = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuffer response = new StringBuffer();
+        String input;
+        while((input = requestResponse.readLine()) != null){
+            response.append(input);
+        }
+        response.deleteCharAt(0);
+        response.deleteCharAt(response.length() - 1);
+        jsonCurseGBP = new JSONObject(response.toString());
+        return (jsonCurseGBP.get("rate").toString().substring(0, jsonCurseGBP.get("rate").toString().length() - 4));
+    }
+}
+
